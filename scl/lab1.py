91e750b9b2a76f3ed54233173157afc616ac5cdf
Justine Major <jumaj7@ulaval.ca>
2021-10-20 17:39:28 -0400

    added instructive comments

--- scl/lab1.py
+++ scl/lab1.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as mpl
 import glob
 
 
+# Useful instances
 results = 18*[0]
 freqs = 18*[0]
 ampl_u = 0.5
 ampl_y = []
 phase = []
 
+# Reading the files
 for file in glob.glob("*.mat"):
     mat = scipy.io.loadmat(file)
     match = re.match(r"w(\d+)_(\d+)_(\d).mat", file)
@@ -22,11 +24,11 @@ for file in glob.glob("*.mat"):
         results[int(match[1])] = result
         freqs[int(match[1])] = freq
 
-
+# Sinus function for curvefitting
 def sinus(t, a, w, phi, b):
     return a * np.sin(w*t + phi) + b
 
-
+# Setting up signal graphs
 setup = np.arange(0, 18, 1).reshape(6, 3)
 fig1, setup = mpl.subplots(6, 3)
 
@@ -43,12 +45,13 @@ for i, trio in enumerate(setup):
         ax.legend()
         index += 1
 
-
+# Making sure every sinus has a positive gain
 for i, amp in enumerate(ampl_y):
     if amp < 0:
         phase[i] = phase[i] - np.pi
         ampl_y[i] = -amp
 
+# Nyquist graph
 Re = []
 Im = []
 
@@ -69,11 +72,12 @@ ax4.set_xlabel('Re')
 ax4.set_ylabel('Im')
 fig4.savefig('Nyquist')
 
-
+# Setting important variables: gain and phase
 ampl_y = np.array(ampl_y)
-rapport = 20*np.log10(ampl_y/ampl_u)
+gain = 20 * np.log10(ampl_y / ampl_u)
 phase = (np.array(phase))*180/2/np.pi
 
+# Bode diagrams (Phase & gain)
 fig2, (ax1, ax2) = mpl.subplots(2, 1)
 ax1.axhline(y=0, color='k')
 ax1.semilogx(freqs, phase)
@@ -85,25 +89,27 @@ ax1.set_xlabel('\u03C9 [rad/s]')
 ax1.set_ylabel('Phase [\u00B0]')
 
 ax2.axhline(y=0, color='k')
-ax2.semilogx(freqs, rapport)
+ax2.semilogx(freqs, gain)
 ax2.minorticks_on()
 ax2.grid(which='major', linestyle='-', linewidth='0.5', color='black')
 ax2.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 ax2.set_xlabel('\u03C9 [rad/s]')
-ax2.set_ylabel("Rapport d'amplitudes [dB]")
+ax2.set_ylabel("Gain [dB]")
 fig2.savefig('Bode')
 
+# Black graph
 fig3, ax3 = mpl.subplots()
 ax3.axhline(y=0, color='k')
 ax3.axvline(x=0, color='k')
-ax3.plot(phase, rapport)
+ax3.plot(phase, gain)
 ax3.minorticks_on()
 ax3.grid(which='major', linestyle='-', linewidth='0.5', color='black')
 ax3.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 ax3.set_title('Lieu de Black')
 ax3.set_xlabel('Phase [\u00B0]')
-ax3.set_ylabel("Rapport d'amplitudes [dB]")
+ax3.set_ylabel("Gain [dB]")
 fig3.savefig('Black')
 
+# show
 mpl.tight_layout()
 mpl.show()
