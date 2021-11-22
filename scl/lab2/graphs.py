7cbf6c80608dceb3b8212c9fcb1b841b53003da3
Justine Major <jumaj7@ulaval.ca>
2021-11-22 13:16:05 -0500

    finished lab2

--- scl/lab2/graphs.py
+++ scl/lab2/graphs.py
@@ -7,11 +7,10 @@ import glob
 
 
 # Useful instances
-t = []
-u = []
+t = {}
+u = {}
 y_exp = {}
 y_th = {}
-doIt = 1
 
 # Reading the files (with regular expressions!! :))
 for file in glob.glob("*.txt"):
@@ -22,22 +21,33 @@ for file in glob.glob("*.txt"):
     for i in mat:
         elem_str = i.replace("\n", "")
         elem = elem_str.split(",")
-        if doIt:
-            t.append(float(elem[0]))
-            u.append(float(elem[1]))
+        t[int(match[1])] = t.get(int(match[1]), []) + [float(elem[0])]
+        u[int(match[1])] = u.get(int(match[1]), []) + [float(elem[1])]
         if match[2] == "exp":
             y_exp[int(match[1])] = y_exp.get(int(match[1]), []) + [float(elem[3])]
         else:
             y_th[int(match[1])] = y_th.get(int(match[1]), []) + [float(elem[3])]
-    doIt = 0
+
+for key, val in t.items():
+    t[key] = t[key][:int(len(val)/2)]
+    u[key] = u[key][:int(len(val)/2)]
 
 fig, ax = mpl.subplots(1)
-ax.plot(t, u, label="$V_{wf}$ expérimental")
-ax.plot(t, y_exp[1], label="$V_{wf}$ obtenu avec IdentSyst")
-ax.plot(t, y_th[1], label="$V_{wf}$ théorique")
+ax.plot(t[1], u[1], label="$V_{wf}$ expérimental")
+ax.plot(t[1], y_exp[1], label="$V_{wf}$ obtenu avec IdentSyst")
+ax.plot(t[1], y_th[1], label="$V_{wf}$ théorique")
 ax.legend()
 ax.set_ylabel('Potentiel [V]')
 ax.set_xlabel('Temps [s]')
 fig.savefig(f"5_1d")
+
+fig2, ax2 = mpl.subplots(1)
+ax2.plot(t[4], u[4], label="$V_{\u03B8}$ expérimental")
+ax2.plot(t[4], y_exp[4], label="$V_{\u03B8}$ obtenu avec IdentSyst")
+ax2.plot(t[4], y_th[4], label="$V_{\u03B8}$ théorique")
+ax2.legend()
+ax2.set_ylabel('Potentiel [V]')
+ax2.set_xlabel('Temps [s]')
+fig2.savefig(f"5_4d")
 
 # mpl.show()
--- scl/lab2/lab2.py
+++ scl/lab2/lab2.py
@@ -33,7 +33,7 @@ for ind, el in enumerate(results[2][0]):
         yes = 0
 
 moy = np.mean(results[2][2][indDeb+100:])
-print(moy)
+# print(moy)
 
 """index = 1
 for i, pair in enumerate(setup):
@@ -47,13 +47,15 @@ for i, pair in enumerate(setup):
             ax.legend()
             index += 1"""
 
-syst = ['ystème en vitesse sans frottement ni inertie', 'ystème en vitesse avec inertie supplémentaire', 'ystème en vitesse avec frottement proportionnel à la vitesse', 'ystème en position']
-num = ['5_1b', '5_2a', '5_3a', '5_4b']
-for index in range(2, 6):
+syst = ['mesuré en 4.1', 'en vitesse sans frottement ni inertie', 'en vitesse avec inertie supplémentaire']
+syst += ['en vitesse avec frottement proportionnel à la vitesse', 'en position']
+num = ['5_5mod', '5_1b', '5_2a', '5_3a', '5_4b']
+for index in range(1, 6):
     result = results[index]
     fig2, ax1 = mpl.subplots(1)
-    # ax1.set_title("Réponse d'un s"+syst[index-2])
+    # ax1.set_title("Réponse d'un système "+syst[index-2])
     ax1.set_xlabel('Temps [s]')
+    # ax1.axhline(y=0, linestyle=':', color='k')
     if index == 5:
         ax1.plot(result[0], result[1], 'r-', label='$V_a$(t)')
         ax1.plot(result[0], result[2], 'b-', label="$V_{\u03B8}$(t)")
@@ -62,8 +64,34 @@ for index in range(2, 6):
         ax1.plot(result[0][100:] - 1, result[1][100:], 'r-', label='$V_a$(t)')
         ax1.plot(result[0][100:] - 1, result[2][100:], 'b-', label="$V_{wf}$(t)")
         ax1.set_ylabel('Potentiel [V]')
+    # ax1.minorticks_on()
+    # ax1.grid(which='major', linestyle='-', linewidth='0.5', color='black')
+    # ax1.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
     ax1.legend()
-    fig2.savefig(f'{num[index-2]}')
+    fig2.savefig(f'{num[index-1]}')
+
+resultX = results[1][1][501:]
+resultY = results[1][2][501:]
+y = []
+x = []
+for i in range(int(len(resultX)/1000)):
+    y.append(np.mean(resultY[i*1000+600:(i+1)*1000-1]))
+    x.append(resultX[i*1000+1])
+
+y += [y[-1]]*2
+x += [4, 5]
+
+fig3, ax3 = mpl.subplots(1)
+ax3.plot(x, y, "o-")
+ax3.set_xlabel("$V_a$ [V]")
+ax3.set_ylabel("$V_{wf}$ [V]")
+ax3.minorticks_on()
+ax3.grid(which='major', linestyle='-', linewidth='0.5', color='black')
+ax3.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+mpl.savefig("5_5a")
+
+# print(results[5][1][50])
+# print(results[5][1][1250])
 
 # Show figures
 mpl.tight_layout()
