85b8e1c10892ba0f369b685e3442096de4cd4e63
Justine Major <jumaj7@ulaval.ca>
2022-04-24 16:42:48 -0400

    dev4q2 quantique

--- quantique/dev4q2.py
+++ quantique/dev4q2.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 # Parameters
 pts = 625
-x_range = 25
-y_range = 25
+x_range = 10
+y_range = 10
 colors_in_quiver = 20
 headwidth=5.0
 minlength=3
@@ -14,13 +14,13 @@ pivot='tail'
 # Initialization
 y,z = np.linspace(-x_range,x_range,pts), np.linspace(-y_range,y_range, pts)
 y,z = np.meshgrid(y,z)
-x = 5
+x = 0
 r = np.sqrt(x**2+y**2+z**2)
 phi = np.arctan(y/x)
 theta = np.arctan(np.sqrt(x**2+y**2)/z)
 
 # Component of P
-comp = (16*np.pi)**-1*r**2*np.exp(-r)*np.sin(theta)**2*np.cos(phi)**2
+comp = (16*np.pi)**-1*r**2*np.exp(-r)*np.sin(theta)**2  # *np.sin(phi)**2
 
 # Plot 
 fig, ax = plt.subplots(figsize=(10,9))
@@ -31,5 +31,5 @@ plt.yticks([])
 ax.set_xlabel('Y')
 ax.set_ylabel('Z')
 
-plt.savefig('x0.pdf')
+plt.savefig('x0-.pdf')
 plt.show()
